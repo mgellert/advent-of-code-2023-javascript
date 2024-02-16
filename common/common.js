11140a629ab5594858f440209425ac2b02e46018
gellertm <mgellert@users.noreply.github.com>
2024-02-16 14:13:31 +0100

    Added standard style and autoformatted.

--- common/common.js
+++ common/common.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs'
 
-export function readLines(path) {
-    const data = fs.readFileSync(path, 'utf8');
-    return data.split(/\n/)
+export function readLines (path) {
+  const data = fs.readFileSync(path, 'utf8')
+  return data.split(/\n/)
 }
--- common/common.test.js
+++ common/common.test.js
@@ -1,8 +1,8 @@
 import { describe, expect, test } from 'vitest'
 import { readLines } from './common'
 
-describe("common functions", () => {
-    test("should read lines of file", () => {
-        expect(readLines("./common/test.txt")).toStrictEqual(["Lorem ipsum", "dolor sit amet."])
-    })
+describe('common functions', () => {
+  test('should read lines of file', () => {
+    expect(readLines('./common/test.txt')).toStrictEqual(['Lorem ipsum', 'dolor sit amet.'])
+  })
 })
