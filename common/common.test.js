5f133147e374a59ed4f0cb515c354d74499d384d
gellertm <mgellert@users.noreply.github.com>
2024-02-16 14:03:37 +0100

    Solved day 1.

---
+++ common/common.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest'
+import { readLines } from './common'
+
+describe("common functions", () => {
+    test("should read lines of file", () => {
+        expect(readLines("./common/test.txt")).toStrictEqual(["Lorem ipsum", "dolor sit amet."])
+    })
+})
