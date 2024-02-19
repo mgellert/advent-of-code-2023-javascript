8f69198c3d6e1201da417b2ffb557b8371f64776
gellertm <mgellert@users.noreply.github.com>
2024-02-19 16:46:35 +0100

    Solved day 5.

--- common/common.js
+++ common/common.js
@@ -4,3 +4,7 @@ export function readLines (path) {
   const data = fs.readFileSync(path, 'utf8')
   return data.split(/\n/)
 }
+
+export function readAll (path) {
+  return fs.readFileSync(path, 'utf8')
+}
